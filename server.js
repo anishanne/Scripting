7667553bea0bea5db8d74af7e04176d4378f5033
Anish <anishanne1@gmail.com>
2020-05-15 15:37:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,28 +9,10 @@ require("./routes/donate")(app);
 require("./routes/faq")(app);
 require("./routes/redirects")(app);
 
-const webhook = require("webhook-discord")
-const errorlogs = new webhook.Webhook("https://discord.com/api/webhooks/710913886433443890/_W1LJtJrnZjjwRS20E44s7QReU-Gggn4qbajbu3E0liTtwHDu0uKshGciTQ7RJhswuT8")
-
 app.get("*", function(req, res) {
   res.render("404")
 });
 
-const hastebin = require("hastebin-gen")
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('500', {
-      message: err.message,
-      error: err
-    });
-    console.log("O")
-    hastebin(`${err.message} | ${err}`, { extension: "txt" }).then(haste => {
-      console.log("haste")
-      errorlogs.err("500 Error", haste)  
-    }).catch(error => {
-      console.error(error);
-  });
-});
 
 
 var listener = app.listen(process.env.PORT, function() {
