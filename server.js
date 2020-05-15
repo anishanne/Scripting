b2a55d02a30622fb64e2b83ba6846c72a8f51721
Anish <anishanne1@gmail.com>
2020-05-15 15:39:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.set('views', `${__dirname}/public/`)
 
 require("./routes/home")(app);
 require("./routes/about")(app);
